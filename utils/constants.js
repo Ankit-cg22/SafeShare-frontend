4205c630938d923aad9aec9acac15a0653626d92
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-08-15 01:33:28 +0530

    minor changes

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
 export const baseServerUrl = 'https://sscg22.herokuapp.com'
 // export const baseServerUrl = 'http://localhost:5000'
-// export const baseClientUrl = 'https://safe-share-frontend.vercel.app'
-export const baseClientUrl = 'http://localhost:3000'
+export const baseClientUrl = 'https://safe-share-frontend.vercel.app'
+// export const baseClientUrl = 'http://localhost:3000'
